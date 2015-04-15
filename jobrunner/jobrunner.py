0c49ca38229765c248bd869c30e4ff48f71ec5f1
freeman <the.freeman.lab@gmail.com>
2015-04-14 23:00:04 -0400

    Refactored database checks
    - Split core operations into validation and execution
    - Check all pull requests on every pass
    - Use database updates to prevent duplicate checks

--- jobrunner/jobrunner.py
+++ jobrunner/jobrunner.py
@@ -1,17 +1,69 @@
 from github import Github
-import pymongo
 from pymongo import MongoClient
 import boto
 import time
 import os
 
 
-def execute_job(job):
-    #TODO: implement job processing on Spark cluster, assuming this runs locally on same machine(s) as cluster
-    pass
+def validate_job(pull_req):
+
+    # clone from the pull request
+    # pull_req.head.repo.clone_url
+
+    # check for a directory ("/submissions/username/")
+    # check for a info.json file
+    # check for a run.py file
+    # if any of this fails, return False, otherwise return True
+
+    # clean up
+
+    return False
+
+
+def execute_job(pull_req):
+
+    # clone from the pull request
+    # pull_req.head.repo.clone_url
+
+    # cd into the cloned directory
+    # execute the run.py for each test data set, return results
+
+    # dump collected results to a JSON file
+    # clean up
+
+    return False
+
+
+def check_pull_req(pull_req, db, field, func, success_msg, failure_msg):
+
+    id = pull_req.id
+    login = pull_req.user.login
+    entry = db.pull_requests.find_one({"id": id})
+
+    if entry:
+        state = entry[field]
+        if not state:
+            result = func(pull_req)
+            if result:
+                timestamp = int(time.mktime(time.gmtime()))
+                db.pull_requests.update_one({"id": id}, {"$set": {field: True}})
+                db.pull_requests.update_one({"id": id}, {"$set": {field + "_at": timestamp}})
+                # pull_req.create_issue_comment(success_msg)
+                print("Sending msg to github: %s" % success_msg)
+            else:
+                # pull_req.create_issue_comment(failure_msg)
+                print("Sending msg to github: %s" % failure_msg)
+            timestamp = int(time.mktime(time.gmtime()))
+            db.pull_requests.update_one({"id": id}, {"$set": {"last_checked": timestamp}})
+        else:
+            print("Already %s pull request %s from user %s" % (field, id, login))
+    else:
+        raise Exception("Cannot find entry in database")
+
 
 def run_job():
-    print "Connecting to MongoDB at MongoLab as user 'codeneurobot'"
+
+    print("Connecting to MongoDB at MongoLab as user 'codeneurobot'")
     mongo_connect_url = os.environ['MONGO_CONNECT_URL']
     if not mongo_connect_url:
         raise Exception("Mongo connect environment variable not set")
@@ -25,37 +77,57 @@ def run_job():
         raise Exception("Github bot password environment variable not set")
 
     mongo_client = MongoClient(mongo_connect_url)
-    codeneurohackathon = mongo_client.codeneurohackathon
-    last_checked = codeneurohackathon.job_records.find_one({"type": "global_last_checked_record"})
-    after = None
+    db = mongo_client.codeneurohackathon
+    last_checked = db.job_records.find_one({"type": "global_last_checked_record"})
+
     if not last_checked:
         timestamp = int(time.time())
         new_last_checked = {"type": "global_last_checked_record", "timestamp": timestamp}
-        codeneurohackathon.job_records.insert(new_last_checked)
-        last_checked = codeneurohackathon.job_records.find_one({"type": "global_last_checked_record"})
-    print "Connecting to Github as user " + github_bot_username
+        db.job_records.insert(new_last_checked)
+        last_checked = db.job_records.find_one({"type": "global_last_checked_record"})
+
+    print("Connecting to Github as user '" + github_bot_username + "'")
     gitbot = Github(github_bot_username, github_bot_pass)
     neurofinder = gitbot.get_repo('CodeNeuro/neurofinder')
-    new_pull_requests = []
     repo_pull_requests = neurofinder.get_pulls()
 
+    # TODO: check for duplicates
+
+    # loop over all pull requests and update database
+    print("Updating database")
     for pull_req in repo_pull_requests:
-        #only counting never-before seen pull reqs for now
-        pull_req_in_db = codeneurohackathon.pull_requests.find_one({"id": pull_req.id})
-        if not pull_req_in_db:
-            if pull_req.mergeable:
-                merged_status = pull_req.merge()
-                if merged_status.merged:
-                    #WARNING: this is untested as of April 11, 2015. It is a hackathon after all...
-                    pull_req_obj = {"id": pull_req.id, "evaluated": True, "directory": ""}
-                    new_pull_requests.append(pull_req_obj)
-                    codeneurohackathon.pull_requests.insert(pull_req)
 
-    #this is still synchronous, but may need to be async on the cluster
-    for new_pull_req in new_pull_requests:
-        #execute_job is only a stub right now. it should package a job from the code
-        execute_job(new_pull_req)
+        if not pull_req.mergeable:
+            pull_req.create_issue_comment("Cannot be merged, validation failed")
 
+        login = pull_req.user.login
+        id = pull_req.id
+        pull_req_in_db = db.pull_requests.find_one({"id": id})
+
+        if pull_req_in_db:
+            updated_at = int(time.mktime(pull_req.updated_at.utctimetuple()))
+            repo_last_checked = pull_req_in_db['last_checked']
+
+            if repo_last_checked < updated_at:
+
+                print("Validating pull request %s from user %s" % (id, login))
+                db.pull_requests.update_one({'id': id}, {'$set': {'validated': False}})
+                check_pull_req(pull_req, db, "validated", validate_job,
+                               "Validation succeeded", "Validation failed")
+
+                print("Executing pull request %s from user %s" % (id, login))
+                db.pull_requests.update_one({'id': id}, {'$set': {'executed': False}})
+                check_pull_req(pull_req, db, "executed", execute_job,
+                               "Execution succeeded", "Execution failed")
+
+            else:
+                print("Skipping pull request %s from user %s" % (id, login))
+
+        else:
+            print("Adding entry for pull request %s from user %s" % (id, login))
+            payload = {'id': id, 'login': login, 'validated': False, 'executed': False,
+                       'last_checked': 0, 'validated_at': 0, 'executed_at': 0}
+            db.pull_requests.insert_one(payload)
 
 if __name__ == '__main__':
     run_job()
