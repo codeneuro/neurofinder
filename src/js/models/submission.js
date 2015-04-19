69586d0e4288460835f71cb8e3ea5dbea51455f6
Matthew Conlen <mc@mathisonian.com>
2015-04-19 17:39:30 -0400

    find dataset names automatically

--- src/js/models/submission.js
+++ src/js/models/submission.js
@@ -16,11 +16,20 @@ var Submission = AmpersandModel.extend({
 
 
     getMetricValueForDataset: function(metric, dataset) {
-        console.log('getting metric ' + metric + ' for dataset ' + dataset);
-        console.log(this);
         var results = this.metrics[metric];
-        console.log(_.findWhere(results, {dataset: dataset}).value);
         return _.findWhere(results, {dataset: dataset}).value;
+    },
+
+    getDatasets: function() {
+        var datasets = [];
+        _.each(this.metrics, function(results) {
+            _.each(results, function(result) {
+                if(result.dataset !== 'overall' && datasets.indexOf(result.dataset) === -1) {
+                    datasets.push(result.dataset);
+                }    
+            });
+        });
+        return datasets;
     }
 });
 
