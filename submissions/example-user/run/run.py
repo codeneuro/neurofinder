1002136a1681306f6d3bfdae9ecb8d7bc671e927
freeman <the.freeman.lab@gmail.com>
2015-04-19 23:56:59 -0400

    Add input arguments to readme

--- submissions/example-user/run/run.py
+++ submissions/example-user/run/run.py
@@ -1,6 +1,16 @@
 def run(data):
     """
     Run a source extraction algorithm on an Images object
+
+    Parameters
+    ----------
+    data : thunder.rdds.images.Images
+        The data the algorithm will be run on
+
+    Returns
+    -------
+    result : thunder.extraction.source.SourceModel
+        Sources found by the algorithm as a SourceModel object
     """
     from thunder import SourceExtraction
     method = SourceExtraction('localmax')
