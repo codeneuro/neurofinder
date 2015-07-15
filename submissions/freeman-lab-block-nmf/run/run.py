3cd922b5382256be97e5964697946bd263b77e54
freeman <the.freeman.lab@gmail.com>
2015-07-15 19:27:10 -0400

    Update test submission

--- submissions/freeman-lab-block-nmf/run/run.py
+++ submissions/freeman-lab-block-nmf/run/run.py
@@ -3,6 +3,8 @@ def run(data):
     Run a Thunder source extraction algorithm
     """
     from thunder import SourceExtraction
-    method = SourceExtraction('nmf')
-    result = method.fit(data, size=(50, 50))
+    from thunder.extraction import OverlapBlockMerger
+    merger = OverlapBlockMerger(0.1)
+    method = SourceExtraction('nmf', merger=merger, componentsPerBlock=5, percentile=95, minArea=100, maxArea=500)
+    result = method.fit(data, size=(32, 32), padding=8)
     return result
