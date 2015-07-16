3b98df76db66b8b051ab3a1966cf112d4a001d28
freeman <the.freeman.lab@gmail.com>
2015-07-16 01:45:36 -0400

    Change parallelism

--- neurofinder/job.py
+++ neurofinder/job.py
@@ -262,7 +262,8 @@ class Job(object):
 
                 data_path = 's3n://' + base_path + '/' + name
                 data_info = self.load_info(base_path, name)
-                data = tsc.loadImages(data_path + '/images/', recursive=True)
+                data = tsc.loadImages(data_path + '/images/', recursive=True,
+                                      npartitions=tsc._sc.defaultParallelism*2)
                 truth = tsc.loadSources(data_path + '/sources/sources.json')
                 sources = run.run(data, info=data_info)
 
