436255dd343b76b691baec408cf6a4042509ef97
freeman <the.freeman.lab@gmail.com>
2015-10-13 16:42:59 -0400

    Update zip script

--- local/zip.py
+++ local/zip.py
@@ -1,10 +1,18 @@
 # /usr/bin/pyhon
 
 name = sys.argv[1]
+
+print('downloading data\n')
 os.system('s4cmd.py get -r s3://neuro.datasets/challenges/neurofinder/%s' % name)
-os.system('cp README.md %s/' % name)
-os.system('cp example.py %s/' % name)
-os.system('cp example.m %s/' % name)
+
+print('packaging data')
+os.system('cp neurofinder/local/README.md %s/' % name)
+os.system('cp neurofinder/local/example.py %s/' % name)
+os.system('cp neurofinder/local/example.m %s/' % name)
 os.system('mv %s neurofinder.training.%s' % name)
+
+print('creating zip')
 os.system('zip -r neurofinder.training.%s.zip neurofinder.training.%s' % (name, name))
+
+print('copying zip to s3')
 os.system('s4cmd.py put neurofinder.training.%s.zip s3://neuro.datasets/challenges/neurofinder/%s/ ' % (name, name))
