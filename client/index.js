ea98c2649182e897c7a45023cace3fd7490d1668
freeman <the.freeman.lab@gmail.com>
2016-03-22 14:48:11 -0400

    fix missing sample

--- client/index.js
+++ client/index.js
@@ -4,8 +4,6 @@ var reducer = require('./reducers')
 var initial = require('./reducers/initial')
 var actions = require('./reducers/actions')
 var components = require('./components')
-var sample1 = require('./sample1')
-var sample2 = require('./sample2')
 
 var store = createStore(reducer, initial)
 hxdx.render(components, store)
